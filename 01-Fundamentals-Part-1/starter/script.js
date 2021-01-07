d45f497925e390e2fdde1d9ef6cb161181645344
Zenith Mabunda <zenith.mabunda01@gmail.com>
2021-01-07 11:53:16 +0200

    challenge 1-check higher BMI

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,3 +5,7 @@ let johnHeight = 1.95;
 
 let markBMI = markMass / markHeight ** 2;
 let johnBMI = johnMass / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
