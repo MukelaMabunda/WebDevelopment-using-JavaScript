46941e77f4f55148b03320bffd3c809c1ade906d
Zenith Mabunda <zenith.mabunda01@gmail.com>
2021-01-08 10:39:12 +0200

    challenge 3-compare averages

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -9,3 +9,7 @@ const koalasScore3 = 110;
 const dolphinsAverage = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3;
 const koalasAverage = (koalasScore1 + koalasScore2 + koalasScore3) / 3;
 
+console.clear();
+if (dolphinsAverage > koalasAverage) { console.log(`Dolphins win score: ${dolphinsAverage}`); }
+else if (dolphinsAverage < koalasAverage) { console.log(`Koalas win score: ${koalasAverage}`); }
+else { console.log(`Draw score: ${koalasAverage}`); }
