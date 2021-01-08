79e9e42208286b18942ec062c8a72bc9fcbe1421
Zenith Mabunda <zenith.mabunda01@gmail.com>
2021-01-08 17:21:02 +0200

    challenge 5

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,3 @@
 'use sctrict';
 console.clear();
-//challenge 4
-
-const calcAverage = (score1,score2,score3) => {
-    return ((score1 + score2 + score3) / 3);
-}
-
-const dolphinsAverage=(calcAverage(96,108,89));
-const koalasAverage=(calcAverage(88,91,110));
-
+//challenge 5
