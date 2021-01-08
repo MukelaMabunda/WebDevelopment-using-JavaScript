c25900b1c8f02cb941bbf0ea412898401d5c1a7e
Zenith Mabunda <zenith.mabunda01@gmail.com>
2021-01-08 10:24:04 +0200

    challenge 3-compute average

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -6,4 +6,6 @@ const koalasScore1 = 88;
 const koalasScore2 = 91;
 const koalasScore3 = 110;
 
+const dolphinsAverage = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3;
+const koalasAverage = (koalasScore1 + koalasScore2 + koalasScore3) / 3;
 
