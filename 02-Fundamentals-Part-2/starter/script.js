44db664f66be7291d808d485b1afbc0b09c5d883
Zenith Mabunda <zenith.mabunda01@gmail.com>
2021-01-08 16:59:57 +0200

    challenge 4-get average using arrow function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,9 @@
 'use sctrict';
+console.clear();
 //challenge 4
+
+const calcAverage = (score1,score2,score3) => {
+    return ((score1 + score2 + score3) / 3);
+}
+
+console.log(calcAverage(1,1,1));
